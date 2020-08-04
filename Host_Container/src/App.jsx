830f79cf52cd39b7981983b92b5154dfb6be0b10
Abhijit H Kulkarni <abhijit.hkulkarni@accoliteindia.com>
2020-08-04 14:01:52 +0530

    Final React Container

---
+++ Host_Container/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => <div><h1>Hi there, I'm React Host.</h1></div>;
+
+ReactDOM.render(<App />, document.getElementById("app"));
